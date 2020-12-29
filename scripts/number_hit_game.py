b94c641ff22e541c14865636132a46542ef65c43
Ryo Nakamura <s19c1138kg@s.chibakoudai.jp>
2020-12-29 01:32:39 +0000

    license

--- scripts/number_hit_game.py
+++ scripts/number_hit_game.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+"""
+BSD 3-Clause License
+
+Copyright (c) 2020, nakamura ryo & Ryuichi Ueda.
+All rights reserved.
+"""
+
 import rospy
 import random
 from std_msgs.msg import Int32
